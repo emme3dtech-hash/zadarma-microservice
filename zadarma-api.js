08a0018e218d79ca229d2cb88e366fccb836ec4f
emme3dtech-hash <emme3dtech@gmail.com>
2025-09-21 21:26:54 +0300

    Update zadarma-api.js

--- zadarma-api.js
+++ zadarma-api.js
@@ -10,49 +10,81 @@ class ZadarmaAPI {
         this.apiUrl = sandbox ? 'api-sandbox.zadarma.com' : 'api.zadarma.com';
     }
 
-    generateSignature(method, path, params = {}) {
+    generateSignature(method, params = {}) {
+        // 1. Сортируем параметры по ключу в алфавитном порядке
         const sortedKeys = Object.keys(params).sort();
         const sortedParams = {};
         sortedKeys.forEach(key => {
             sortedParams[key] = params[key];
         });
         
+        // 2. Создаем query string (аналогично http_build_query в PHP)
         const queryString = querystring.stringify(sortedParams);
-        const stringToSign = method + path + queryString + this.key;
         
-        console.log('=== DEBUG INFO ===');
+        // 3. Создаем MD5 хеш от query string
+        const md5Hash = crypto.createHash('md5').update(queryString).digest('hex');
+        
+        // 4. Создаем строку для подписи: method + queryString + md5(queryString)
+        const stringToSign = method + queryString + md5Hash;
+        
+        console.log('=== ZADARMA AUTH DEBUG ===');
         console.log('Method:', method);
-        console.log('Path:', path);
-        console.log('QueryString:', queryString);
-        console.log('API Key:', this.key);
+        console.log('Sorted params:', sortedParams);
+        console.log('Query string:', queryString);
+        console.log('MD5 hash:', md5Hash);
         console.log('String to sign:', stringToSign);
+        console.log('API Key:', this.key);
         
+        // 5. Создаем HMAC SHA1 подпись с секретным ключом
         const signature = crypto
             .createHmac('sha1', this.secret)
             .update(stringToSign)
             .digest('base64');
             
         console.log('Generated signature:', signature);
-        console.log('=================');
+        console.log('Authorization header:', this.key + ':' + signature);
+        console.log('========================');
         
         return signature;
     }
 
     makeRequest(method, path, params = {}) {
         return new Promise((resolve, reject) => {
-            const signature = this.generateSignature(method, path, params);
-            const queryString = Object.keys(params).length > 0 ? '?' + querystring.stringify(params) : '';
+            const signature = this.generateSignature(method, params);
+            
+            let requestPath = path;
+            let postData = '';
             
             const options = {
                 hostname: this.apiUrl,
                 port: 443,
-                path: path + queryString,
                 method: method,
                 headers: {
                     'Authorization': this.key + ':' + signature,
                     'Content-Type': 'application/x-www-form-urlencoded'
                 }
             };
+
+            // Для GET запросов добавляем параметры в URL
+            if (method === 'GET' && Object.keys(params).length > 0) {
+                const queryString = querystring.stringify(params);
+                requestPath = path + '?' + queryString;
+            }
+            
+            // Для POST запросов добавляем параметры в body
+            if (method === 'POST' && Object.keys(params).length > 0) {
+                postData = querystring.stringify(params);
+                options.headers['Content-Length'] = Buffer.byteLength(postData);
+            }
+            
+            options.path = requestPath;
+            
+            console.log('=== REQUEST INFO ===');
+            console.log('URL:', `https://${this.apiUrl}${requestPath}`);
+            console.log('Method:', method);
+            console.log('Headers:', options.headers);
+            if (postData) console.log('Post data:', postData);
+            console.log('==================');
 
             const req = https.request(options, (res) => {
                 let data = '';
@@ -62,6 +94,11 @@ class ZadarmaAPI {
                 });
                 
                 res.on('end', () => {
+                    console.log('=== RESPONSE INFO ===');
+                    console.log('Status code:', res.statusCode);
+                    console.log('Response body:', data);
+                    console.log('====================');
+                    
                     try {
                         const jsonData = JSON.parse(data);
                         resolve({
@@ -69,7 +106,7 @@ class ZadarmaAPI {
                             data: jsonData
                         });
                     } catch (error) {
-                        reject(new Error('Ошибка парсинга JSON: ' + error.message));
+                        reject(new Error('Ошибка парсинга JSON: ' + error.message + '. Raw response: ' + data));
                     }
                 });
             });
@@ -78,6 +115,11 @@ class ZadarmaAPI {
                 reject(new Error('Ошибка запроса: ' + error.message));
             });
 
+            // Отправляем POST данные, если есть
+            if (postData) {
+                req.write(postData);
+            }
+            
             req.end();
         });
     }
@@ -95,11 +137,14 @@ class ZadarmaAPI {
         try {
             const params = {
                 from: from,
-                to: to,
-                predicted: predicted ? 1 : 0
+                to: to
             };
             
-            const response = await this.makeRequest('POST', '/v1/request/callback/', params);
+            if (predicted) {
+                params.predicted = 'predicted';
+            }
+            
+            const response = await this.makeRequest('GET', '/v1/request/callback/', params);
             return response;
         } catch (error) {
             throw new Error('Ошибка инициации обратного звонка: ' + error.message);
@@ -108,10 +153,37 @@ class ZadarmaAPI {
 
     async getNumbers() {
         try {
-            const response = await this.makeRequest('GET', '/v1/info/numbers/');
+            const response = await this.makeRequest('GET', '/v1/direct_numbers/');
             return response;
         } catch (error) {
             throw new Error('Ошибка получения номеров: ' + error.message);
+        }
+    }
+
+    async sendSMS(number, message, caller_id = null) {
+        try {
+            const params = {
+                number: number,
+                message: message
+            };
+            
+            if (caller_id) {
+                params.caller_id = caller_id;
+            }
+            
+            const response = await this.makeRequest('POST', '/v1/sms/send/', params);
+            return response;
+        } catch (error) {
+            throw new Error('Ошибка отправки SMS: ' + error.message);
+        }
+    }
+
+    async getTariffs() {
+        try {
+            const response = await this.makeRequest('GET', '/v1/tariff/');
+            return response;
+        } catch (error) {
+            throw new Error('Ошибка получения тарифов: ' + error.message);
         }
     }
 }
